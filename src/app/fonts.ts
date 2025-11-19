78a0b05b99fc35b47f0b250eb60744a6a8e4ff75
ryeong <oaoa0728@naver.com>
2025-11-19 23:55:04 +0900

    style: setup Pretendard font

---
+++ src/app/fonts.ts
@@ -0,0 +1,28 @@
+import localFont from 'next/font/local';
+
+export const pretendard = localFont({
+  src: [
+    {
+      path: 'fonts/Pretendard-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Pretendard-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Pretendard-SemiBold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Pretendard-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-pretendard',
+  display: 'swap',
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import { pretendard } from './fonts';
 
 export const metadata: Metadata = {
   title: 'ryeong',
@@ -12,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={pretendard.variable}>
       <body>{children}</body>
     </html>
   );
