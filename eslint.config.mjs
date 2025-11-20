b5c65208dbe8ba3b6a2c64716a37fa591f502043
ryeong <oaoa0728@naver.com>
2025-11-20 17:37:11 +0900

    chore: add ESLint + Prettier integration config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,28 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  prettierConfig,
+
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+
+    rules: {
+      'no-console': 'warn',
+      'no-unused-vars': 'warn',
+      'prettier/prettier': 'error',
+    },
+  },
+
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
 ]);
 
 export default eslintConfig;
